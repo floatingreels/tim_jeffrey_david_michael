5f9e3a5aa7328c7221342246a5e782e8f50e8009
mvdw0024 <m.vdw@live.be>
2019-12-16 15:03:40 +0100

    Made Product (Entity) Class

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/Product.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/Product.java
@@ -1,4 +1,78 @@
 package com.floatingreels.tim_jeffrey_david_michael.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank
+    @NotNull
+    private String name, imgurl, category;
+    @Size(min = 100, message = "give a proper description of this product")
+    private String description;
+    @DecimalMin(value = "0.05")
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    /* geschreven door Michael */
+
 }
