005f1f4c0dbcdc221a92289df8f1ae1be58cc523
mvdw0024 <m.vdw@live.be>
2019-12-16 16:16:36 +0100

    Fixed Queries Made adaptions to index.html

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/ProductDAO.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/ProductDAO.java
@@ -7,13 +7,13 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProductDAO extends CrudRepository<Product, Integer> {
 
-    @Query("SELECT b FROM Product b WHERE b.category=FOOD")
+    @Query("SELECT b FROM Product b WHERE b.category='FOOD'")
     Iterable<Product> getCatFood();
 
-    @Query("SELECT b FROM Product b WHERE b.category=TOY")
+    @Query("SELECT b FROM Product b WHERE b.category='TOY'")
     Iterable<Product> getCatToy();
 
-    @Query("SELECT b FROM Product b WHERE b.category=HABITAT")
+    @Query("SELECT b FROM Product b WHERE b.category='HABITAT'")
     Iterable<Product> getCatHabitat();
 
 
