8d84e9988dec05c97b1f2ad3f1b413f49ec31155
floatingreels <david.gunzburg@gmail.com>
2019-12-16 14:09:28 +0100

    Initial commit Blank Gradle project

---
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/TimJeffreyDavidMichaelApplication.java
@@ -0,0 +1,13 @@
+package com.floatingreels.tim_jeffrey_david_michael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimJeffreyDavidMichaelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimJeffreyDavidMichaelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/floatingreels/tim_jeffrey_david_michael/TimJeffreyDavidMichaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floatingreels.tim_jeffrey_david_michael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimJeffreyDavidMichaelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
