b7abc2fa10e3653e3af42f6199aec16fc3960aea
mvdw0024 <m.vdw@live.be>
2019-12-16 15:16:00 +0100

    Made Product (Entity) Class

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/ProductDAO.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/ProductDAO.java
@@ -3,8 +3,18 @@ package com.floatingreels.tim_jeffrey_david_michael.model;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+/*author: Michael */
+
 public interface ProductDAO extends CrudRepository<Product, Integer> {
-    @Query("SELECT b FROM Blog b ORDER BY b.Ca DESC")
-    Iterable<Product> getCatVoeding();
+
+    @Query("SELECT b FROM Product b WHERE b.category=FOOD")
+    Iterable<Product> getCatFood();
+
+    @Query("SELECT b FROM Product b WHERE b.category=TOY")
+    Iterable<Product> getCatToy();
+
+    @Query("SELECT b FROM Product b WHERE b.category=HABITAT")
+    Iterable<Product> getCatHabitat();
+
 
 }
