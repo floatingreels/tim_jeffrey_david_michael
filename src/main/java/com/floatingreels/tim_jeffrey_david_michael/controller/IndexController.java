e9e4757776b5c5c1ecad23f551e8f3db4acdd980
floatingreels <david.gunzburg@gmail.com>
2019-12-16 15:26:53 +0100

    Added RequestMapping for Index Added ModelAttribute to show all products

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/controller/IndexController.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/controller/IndexController.java
@@ -1,4 +1,31 @@
 package com.floatingreels.tim_jeffrey_david_michael.controller;
 
+import com.floatingreels.tim_jeffrey_david_michael.model.Product;
+import com.floatingreels.tim_jeffrey_david_michael.model.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+//added by DG
+@RequestMapping(value="/tj")
+@Controller
 public class IndexController {
+    @Autowired
+    ProductDAO dao;
+
+    @RequestMapping(value={"", "/index"}, method = RequestMethod.GET)
+    public String showIndex(ModelMap map){
+        return "index";
+    }
+
+    @ModelAttribute(value="allProducts")
+    public Iterable<Product> showProducts(){
+        return dao.findAll();
+    }
+
+
+
 }
