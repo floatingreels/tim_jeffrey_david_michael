10d4ebeb63017258d8eb8fa6ede003adb1687096
floatingreels <david.gunzburg@gmail.com>
2019-12-19 13:48:58 +0100

    furthered internationalisation

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/controller/IndexController.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/controller/IndexController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 //added by DG
 @Controller
 public class IndexController {
