101525fc0ed842a40e6cf2aa8394b0e4077efc1a
floatingreels <david.gunzburg@gmail.com>
2019-12-19 17:06:40 +0100

    Fixed Delete function by adding equals() to specify what fields must me alike for object to be alike

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/Product.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/Product.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 public class Product {
@@ -108,4 +109,17 @@ public class Product {
     }
     /* geschreven door Michael */
 
+    //Thanks, Swasse!
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
