b663c381ffec5786aa7cf9b8f232dd4515b6d004
floatingreels <david.gunzburg@gmail.com>
2019-12-20 08:26:20 +0100

    Started adding methods to CustomerController.

--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/controller/ShoppingCartController.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/controller/ShoppingCartController.java
@@ -46,4 +46,5 @@ public class ShoppingCartController {
         cart.clear();
         return "redirect:/confirmation";
     }
+
 }
--- src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/CustomerDAO.java
+++ src/main/java/com/floatingreels/tim_jeffrey_david_michael/model/CustomerDAO.java
@@ -1,7 +1,7 @@
-//package com.floatingreels.tim_jeffrey_david_michael.model;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface CustomerDAO extends CrudRepository<Customer, Integer> {
-//
-//}
+package com.floatingreels.tim_jeffrey_david_michael.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerDAO extends CrudRepository<Customer, Integer> {
+
+}
